8461cc47bca7746fe1e296dacf94527a830a4fe4
Andrey Rusak <dudzy@mail.ru>
2023-12-09 01:06:22 +0300

    Добавил проверки в тесты

--- tests/test_add_to_archive.py
+++ tests/test_add_to_archive.py
@@ -9,13 +9,16 @@ files_dir = os.listdir(TEST_DATA_DIR)
 
 def test_add_files_to_zip_via_shutil():
     shutil._make_zipfile(base_name=f'{TMP_DIR}\hello', base_dir=TEST_DATA_DIR)
-
+    assert os.path.isfile(f'{TMP_DIR}\hello.zip')
 
 def test_add_files_to_zip_via_zipfile():
-    if os.path.isfile('hello.zip'):
-        shutil.rmtree(os.path.join(CURRENT_DIR, "resources"))
+    if os.path.isfile(f'{TMP_DIR}\hello.zip'):
+        shutil.rmtree(TMP_DIR)
+
+    os.mkdir(f'{CURRENT_DIR}\\resources')
 
     with ZipFile(f'{TMP_DIR}\hello.zip', mode='w', compression=ZIP_STORED) as zip_file:
         for file in files_dir:
             add_file = os.path.join(TEST_DATA_DIR, file)
             zip_file.write(add_file)
+    assert os.path.isfile(f'{TMP_DIR}\hello.zip')
